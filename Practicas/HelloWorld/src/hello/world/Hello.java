178df10dd1847fbaf8de36532b87ae6f58a1dabd
SirMathius <mattslightwolf@gmail.com>
2021-06-06 16:17:42 -0300

    Capasiatcion Java seccion 3 comienzo

---
+++ Practicas/HelloWorld/src/hello/world/Hello.java
@@ -0,0 +1,18 @@
+package hello.world;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Hello, Matts");
+		
+	int miPrimerNumero = (10 + 5) + (2 * 10);
+	int miSegundoNumero = 12;
+	int miTercerNumero = miPrimerNumero * 2;
+	int miTotal = miPrimerNumero + miSegundoNumero + miTercerNumero;
+	System.out.println(miTotal);
+	int miUltimoNumero = 1000 - miTotal;
+	System.out.println(miUltimoNumero);
+	
+	}
+}
